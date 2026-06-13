d17d6c4ca2f394562338dff005779f59f7e0585a
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-06-13 17:29:50 +0300

    adding login authentication and implementing a function that compares password

--- server/configs/hashpassword.js
+++ server/configs/hashpassword.js
@@ -5,3 +5,7 @@ export const hashPassword = (password) => {
     const salt = bcrypt.genSaltSync(saltround)
     return bcrypt.hashSync(password,salt)
 }
+
+export const comparepassword = (plain,hashed) => {
+    bcrypt.compareSync(plain,hashed)
+}
--- server/controllers/authenticationController.js
+++ server/controllers/authenticationController.js
@@ -1,3 +1,6 @@
+import conn from "../configs/db.js"
+import { comparepassword, hashPassword } from "../configs/hashpassword.js"
+import jwt from 'jsonwebtoken'
 
 
 export const createAccount = async (request,response) => {
@@ -5,8 +8,41 @@ export const createAccount = async (request,response) => {
     if (name === "" || email === "" || password === "") {
         return response.status(200).json({status:false, message:"Please fill all fields"})
     }
+    const hashpassword = hashPassword(password)
     try {
-        const sqlQuerry = "INSERT INTO user(name,email,password) VALUES(?)"
+        const sqlQuerry = "INSERT INTO user(name,email,password) VALUES(?,?,?)"
+        conn.query(sqlQuerry,[name,email,hashpassword], (error, result) => {
+            console.log(error)
+            if (error) return response.status(200).json({status:false, message:error})
+            return response.status(201).json({status:true, message:"Account created successfully"})
+        })        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const loginAccount = async (request,response) => {
+    const { email, password} = request.body
+    if (!email || !password) {
+        response.status(200).json({status:false, message:"Fill all fields"})
+    }
+    try {
+        const sqlQuerry = "SELECT * FROM user WHERE email = ?"
+        conn.query(sqlQuerry,[email], (error,result) => {
+            if (error) return response.status(200).json({status: false, message: error})
+            if (result.length > 0) {
+                const pass = comparepassword(password, result[0].password)
+                if (pass) {
+                    const name = result[0].name
+                    const token = jwt.sign({name},"login_rtie_owp",{expiresIn:'2d'})
+                    return response.status(200).json({status:true, name:name})
+                } else {
+                    return response.status(200).json({status:false, message:"wrong credentials"})
+                }
+            } else {
+                return response.status(404).json({status:false, message:"user not found"})
+            }
+        })
     } catch (error) {
         console.log(error)
     }
