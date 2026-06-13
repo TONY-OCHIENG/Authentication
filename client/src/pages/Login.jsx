1e69d808b5d30aca994ef6208ea290cf88e40c24
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-06-13 09:39:04 +0300

    Handling login values

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,16 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 function Login() {
+    const [loginDetails,setLoginDetails] = useState({
+        email:'',
+        password:''
+    })
+    const handleLogin = (event) => {
+        event.preventDefault()
+    
+    }
+    const handleValues = (event) => {
+        const { name, value } = event.target 
+        setLoginDetails((prev) => ({
+          ...prev,
+          [name]:value  
+        }))
+    }
+
+    console.log(loginDetails)
   return (
     <div className='bg-gray-100 px-3 w-full h-[100vh] flex justify-center items-center'>
         <div className='w-full md:w-[40%] shadow-md p-4 rounded-md bg-white '>
-            <form action="" className='w-full'>
+            <form action="" onSubmit={handleLogin} className='w-full'>
                 <label htmlFor="email">Email</label>
-                <input type="email" className='w-full p-2 rounded-md border mb-3' id='email' name='email'/>
+                <input  onChange={handleValues} type="email" className='w-full p-2 rounded-md border mb-3' id='email' name='email'/>
                 <label htmlFor="password">Password</label>
-                <input type="password" className='w-full p-2 rounded-md border mb-3' id='password' name='password'/>
+                <input  onChange={handleValues} type="password" className='w-full p-2 rounded-md border mb-3' id='password' name='password'/>
                 <button className='w-full py-3 font-lg font-bold cursor-pointer bg-black text-white
                 rounded-md'>Login</button>
+                <p className='text-gray-600 mt-3 text-xs'>Don't have an Account? <Link to={'/register'} className='font-extrabold'>Click to Register</Link></p>
             </form>
         </div>
     </div>
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Register() {
   return (
-    <div>Register</div>
+    <div className='bg-gray-100 px-3 w-full h-[100vh] flex justify-center items-center'>
+        <div className='w-full md:w-[40%] shadow-md p-4 rounded-md bg-white '>
+            <form action="" className='w-full'>
+                <label htmlFor="name">Name</label>
+                <input type="text" className='w-full p-2 rounded-md border mb-3' id='name' name='name'/>
+                <label htmlFor="email">Email</label>
+                <input type="email" className='w-full p-2 rounded-md border mb-3' id='email' name='email'/>
+                <label htmlFor="password">Password</label>
+                <input type="password" className='w-full p-2 rounded-md border mb-3' id='password' name='password'/>
+                <button className='w-full py-3 font-lg font-bold cursor-pointer bg-black text-white
+                rounded-md'>Register</button>
+                <p className='text-gray-600 mt-3 text-xs'>Already have an Account? <Link to={'/login'} className='font-extrabold'>Click to Login</Link></p>
+            </form>
+        </div>
+    </div>
   )
 }
 
