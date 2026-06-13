1bd82d5a3d2599ede3f598da6bb9f2fed4f58228
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-06-13 15:01:54 +0300

    Encapsulating mysql connection details on dotenv

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,10 +1,11 @@
 import mysql from 'mysql2'
+import 'dotenv/config'
 
 const conn = mysql.createConnection({
-    host:"localhost",
-    user:'root',
-    password:'Tony#895',
-    database:'auth'
+    host:process.env.HOST,
+    user:process.env.USER,
+    password:process.env.PASSWORD,
+    database:process.env.DATABASE
 })
 
 conn.connect((error) => {
