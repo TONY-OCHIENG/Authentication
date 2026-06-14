a84c64ffae3f0021d54a71403de86b305342bab9
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-06-14 18:38:20 +0300

    Adding logout route

--- server/routes/authenticationRoutes.js
+++ server/routes/authenticationRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { createAccount, loginAccount, username } from '../controllers/authenticationController.js'
+import { createAccount, loginAccount, logout, username } from '../controllers/authenticationController.js'
 import { auth } from '../middlewares/auth.js'
 
 const authentication = express.Router()
 authentication.post('/createAccount', createAccount)
 authentication.post('/login',loginAccount)
 authentication.get('/',auth,username)
+authentication.get('/logout',logout)
 export default authentication
